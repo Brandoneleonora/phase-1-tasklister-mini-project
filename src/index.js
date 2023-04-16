cdaf1ba33315a918be642eb44fe2685bd50ad83d
Brandon Eleonora <brandoneleonora38@gmail.com>
2023-04-16 14:28:09 -0400

    complete project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const newTask = document.getElementById('new-task-description');
+
+ let form = document.querySelector('form')
+ form.addEventListener('submit',(e) =>{
+    e.preventDefault()
+    handleTodo(newTask.value);
+
+    form.reset()
+  })
 });
+
+
+function handleTodo(todo){
+  let p = document.createElement('p');
+  let btn = document.createElement('button')
+  btn.addEventListener('click',handleDelete)
+  btn.textContent = 'x'
+  p.textContent = `${todo} `
+   p.appendChild(btn);
+  document.querySelector('#list').appendChild(p)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
